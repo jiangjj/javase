7d882a16363e74e074b955b1a78258f80edfd93a
jiangjj <149521920@qq.com>
2017-01-26 21:21:43 +0800

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/1/26.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
